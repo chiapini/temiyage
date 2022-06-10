21e6e94fd877a608913a5a1930066ee252490b17
chiapini <chiara.rei.filippini@gmail.com>
2022-06-10 17:56:21 +0900

    ビューの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :confiqure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def after_sign_in_path_for(_resource)
+    top_index_path
+  end
+
+  def confiqure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :gender, :situation, :children, :age_id, :job_id, :area_id, :think_id])
+  end
 end
+
