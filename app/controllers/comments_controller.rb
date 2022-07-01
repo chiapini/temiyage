7d61165fe58feee6de30d8c31a0a114537aefc9e
chiapini <chiara.rei.filippini@gmail.com>
2022-07-01 14:24:15 +0900

    コメントの削除と編集を可能とする

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,21 @@ class CommentsController < ApplicationController
     redirect_to tweet_path(params[:tweet_id])
   end
 
+  def edit; end
+
+  def destroy
+    @comment.destroy
+    redirect_to tweets_path
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to tweet_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def comment_params
